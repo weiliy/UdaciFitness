8e1e176ee8872519ec5d95f5b591762f93fa4edb
Weili Yi <wlyi@thoughtworks.com>
2018-08-07 17:33:38 +0800

    add utils

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World, react native!!!</Text>
+        <Text>Hello World, react native!!!<Ionicons name="ios-pizza" color="red" /></Text>
       </View>
     );
   }
