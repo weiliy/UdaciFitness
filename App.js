8b340854fba35e0cbab378147ecc8d9521bfcde8
Weili Yi <wlyi@thoughtworks.com>
2018-08-08 11:34:42 +0800

    AddEntry-getMetricMetaInfo

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World, react native!!!<Ionicons name="ios-pizza" color="red" /></Text>
+      <View>
+        <AddEntry />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fee',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
